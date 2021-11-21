a6d91dd9e8ccca65aa48d82fbc43a28e48339972
Lars <larsi.thestorf@gmail.com>
2021-11-21 23:45:24 +0100

    Changes related to new Controller pcb#

--- Software/xmega/buttonMatrix.c
+++ Software/xmega/buttonMatrix.c
@@ -32,6 +32,30 @@ void ButtonMatrixWorker()
 	PIN_BTNMATRIX_A_reg.OUTCLR = PIN_BTNMATRIX_A_bm;
 	_delay_us(10);
 	if ((PIN_BTNMATRIX_1_reg.IN & PIN_BTNMATRIX_1_bm) == 0)
+		buttonstates |= BUTTON_DD;
+	if ((PIN_BTNMATRIX_2_reg.IN & PIN_BTNMATRIX_2_bm) == 0)
+		buttonstates |= BUTTON_DL;
+	if ((PIN_BTNMATRIX_3_reg.IN & PIN_BTNMATRIX_3_bm) == 0)
+		buttonstates |= BUTTON_X;
+	if ((PIN_BTNMATRIX_4_reg.IN & PIN_BTNMATRIX_4_bm) == 0)
+		buttonstates |= BUTTON_A;
+	PIN_BTNMATRIX_A_reg.OUTSET = PIN_BTNMATRIX_A_bm;
+	
+	PIN_BTNMATRIX_B_reg.OUTCLR = PIN_BTNMATRIX_B_bm;
+	_delay_us(10);
+	if ((PIN_BTNMATRIX_1_reg.IN & PIN_BTNMATRIX_1_bm) == 0)
+		buttonstates |= BUTTON_DR;
+	if ((PIN_BTNMATRIX_2_reg.IN & PIN_BTNMATRIX_2_bm) == 0)
+		buttonstates |= BUTTON_DU;
+	if ((PIN_BTNMATRIX_3_reg.IN & PIN_BTNMATRIX_3_bm) == 0)
+		buttonstates |= BUTTON_Y;
+	if ((PIN_BTNMATRIX_4_reg.IN & PIN_BTNMATRIX_4_bm) == 0)
+		buttonstates |= BUTTON_B;
+	PIN_BTNMATRIX_B_reg.OUTSET = PIN_BTNMATRIX_B_bm;
+	
+	PIN_BTNMATRIX_C_reg.OUTCLR = PIN_BTNMATRIX_C_bm;
+	_delay_us(10);
+	if ((PIN_BTNMATRIX_1_reg.IN & PIN_BTNMATRIX_1_bm) == 0)
 		buttonstates |= BUTTON_L;
 	if ((PIN_BTNMATRIX_2_reg.IN & PIN_BTNMATRIX_2_bm) == 0)
 		buttonstates |= BUTTON_SELECT;
@@ -39,30 +63,6 @@ void ButtonMatrixWorker()
 		buttonstates |= BUTTON_START;
 	if ((PIN_BTNMATRIX_4_reg.IN & PIN_BTNMATRIX_4_bm) == 0)
 		buttonstates |= BUTTON_R;
-	PIN_BTNMATRIX_A_reg.OUTSET = PIN_BTNMATRIX_A_bm;
-	
-	PIN_BTNMATRIX_B_reg.OUTCLR = PIN_BTNMATRIX_B_bm;
-	_delay_us(10);
-	if ((PIN_BTNMATRIX_1_reg.IN & PIN_BTNMATRIX_1_bm) == 0)
-		buttonstates |= BUTTON_DL;
-	if ((PIN_BTNMATRIX_2_reg.IN & PIN_BTNMATRIX_2_bm) == 0)
-		buttonstates |= BUTTON_DD;
-	if ((PIN_BTNMATRIX_3_reg.IN & PIN_BTNMATRIX_3_bm) == 0)
-		buttonstates |= BUTTON_B;
-	if ((PIN_BTNMATRIX_4_reg.IN & PIN_BTNMATRIX_4_bm) == 0)
-		buttonstates |= BUTTON_A;
-	PIN_BTNMATRIX_B_reg.OUTSET = PIN_BTNMATRIX_B_bm;
-	
-	PIN_BTNMATRIX_C_reg.OUTCLR = PIN_BTNMATRIX_C_bm;
-	_delay_us(10);
-	if ((PIN_BTNMATRIX_1_reg.IN & PIN_BTNMATRIX_1_bm) == 0)
-		buttonstates |= BUTTON_DU;
-	if ((PIN_BTNMATRIX_2_reg.IN & PIN_BTNMATRIX_2_bm) == 0)
-		buttonstates |= BUTTON_DR;
-	if ((PIN_BTNMATRIX_3_reg.IN & PIN_BTNMATRIX_3_bm) == 0)
-		buttonstates |= BUTTON_Y;
-	if ((PIN_BTNMATRIX_4_reg.IN & PIN_BTNMATRIX_4_bm) == 0)
-		buttonstates |= BUTTON_X;
 	PIN_BTNMATRIX_C_reg.OUTSET = PIN_BTNMATRIX_C_bm;
 	
 	buttons_cache = buttonstates;
